86d126d0f01360cf9cb9bde4834637f843f00375
jamesfang87 <james.fang.713@gmail.com>
2025-02-01 06:36:02 -0600

    red neg auton

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,15 +1,44 @@
 #include "auton.h"
 
+ASSET(move1_txt);
+ASSET(move2_txt);
+
+ASSET(first_goal_txt);
+
 void blue_neg() {
-    //clamp.toggle();
-    chassis.setPose(42, 60, 0);
-    //chassis.turnToHeading(150, 4000);
-    chassis.moveToPose(60, 20, -35, 4000, {.forwards = false, .maxSpeed = 127, .minSpeed = 70}, false);
-    
+    chassis.setPose(59.814, 36.858, 90);
+    // lookahead distance: 15 inches
+
+    // follow path, backward - drive to stake1
+    chassis.follow(move1_txt, 15, 3000, false, false);
     clamp.toggle();
-    pros::delay(200);
-    
-    //chassis.turnToPoint(48, 24, 1000, {}, false);
-    //chassis.moveToPoint(36, 24, 1000, {}, false);
-    //chassis.turnToHeading(-90, 4000, {}, false);
+    pros::delay(175);
+    intake.move_velocity(12000);
+
+    chassis.follow(move2_txt, 15, 9000, true, false);
+}
+
+void red_neg() {
+    // path, lookahead, time limit, forward, async
+
+    // get the first goal
+    chassis.follow(first_goal_txt, 15, 2000, false, false);
+    clamp.toggle();
+    pros::delay(175);
+    intake.move_velocity(12000);
+
+    // now get the first ring (right above the first goal);
+    chassis.follow(first_ring_txt, 15, 2000, true, false);
+
+    // move back a bit
+    chassis.follow(move_back_txt, 15, 2000, false, false);
+
+    // go for the second ring (closer ring to the middle)
+    chassis.follow(second_ring_txt, 15, 2000, true, false);
+
+    // move back a bit again
+    chassis.follow(move_back2_txt, 15, 2000, false, false);
+
+    // get the last ring
+    chassis.follow(last_ring_txt, 14, 2000, true, false);
 }
