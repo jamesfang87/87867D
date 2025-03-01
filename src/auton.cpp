2956e749e5b921cc4fd8e03696cc27a49489c1ab
2008rjiang <2008rjiang@gmail.com>
2025-02-28 23:22:43 -0600

    red pos idk

--- src/auton.cpp
+++ src/auton.cpp
@@ -7,6 +7,10 @@ void blue_neg() {
     
 }
 
+void blue_pos(){
+    
+}
+
 ASSET(redneg1_txt);
 void red_neg() {
     chassis.setPose(-63, 12, 270);
@@ -29,6 +33,36 @@ void red_neg() {
     //chassis.moveToPoint(-65, 65, 3000, {.maxSpeed = 70}, false);
 
     //chassis.moveToPoint(-48, 0, 3000, {.maxSpeed = 70}, false);
+}
+
+ASSET(redpos1_txt);
+ASSET(redpos2_txt);
+ASSET(redpos3_txt);
+void red_pos(){
+    chassis.setPose(-63, -36, 270);
+    chassis.follow(redpos1_txt, 12, 2500, false, false);
+    clamp.toggle();
+    pros::delay(60);
+    intake.move_voltage(9500);
+    pros::delay(100);
+    clamp.toggle();
+
+    chassis.turnToPoint(-23.5, -48, 800, {.maxSpeed = 90}, false);
+    chassis.moveToPoint(-23.5, -48, 1000, {.maxSpeed = 70}, false);
+    intake.move_voltage(4000);
+    pros::delay(50);
+    intake.break();
+    
+    chassis.follow(redpos2_txt, 12, 2500, false, false);
+    clamp.toggle();
+    // pros::delay(30);
+    intake.move_voltage(9000);
+    pros::delay(80);
+
+    chassis.turnToPoint(-48, 0, 800, {.maxSpeed = 90}, false);
+    intake.break();
+    chassis.follow(redpos3_txt, 12, 3500, false, false);
+
 }
 
 void red_elim() {
