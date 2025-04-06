c8cab9222df098740bb6b6bcd5b60a9abbb19276
jamesfang87 <james.fang.713@gmail.com>
2025-04-05 23:14:06 -0500

    test for 4/6

--- src/skills.cpp
+++ src/skills.cpp
@@ -51,7 +51,7 @@ void skills() {
     intake.move_velocity(330);
     chassis.moveToPose(25, 47, 72, 2000, {.maxSpeed = 90, .minSpeed = 55});
     pros::delay(1200);
-    move_arm_to(20, 800);
+    move_arm_to(22, 800);
 
     // score ring 1 on high stake
     chassis.moveToPoint(0.5, 42, 1500, {.forwards = false, .maxSpeed = 80}, false);
@@ -79,7 +79,6 @@ void skills() {
     intake.brake();
     toggle_clamp();
 
-
     // TODO: Tune
     // move across the field and load ring into arm
     chassis.moveToPoint(24, 50, 1500, {.minSpeed = 90}, false);
@@ -100,7 +99,50 @@ void skills() {
     // move to middle goal
     chassis.moveToPoint(60, 24, 1500, {.maxSpeed = 70}, false);
     chassis.turnToPoint(48, 0, 800, {.maxSpeed = 90}, false);
+    chassis.moveToPoint(48, 0, 800, {.maxSpeed = 60}, false);
+    toggle_clamp();
 
-    pros::lcd::print(5, "left: %i | right: %i", left_motors.get_temperature(), right_motors.get_temperature());
-    pros::lcd::print(5, "arm: %i | intake: %i", arm.get_temperature(), intake.get_temperature());
+    // TODO: score on alliance stake
+
+    // fill up goal
+    intake.move_velocity(360);
+    chassis.moveToPoint(23.5, 23.5, 1500, {.maxSpeed= 80}, false);
+    chassis.turnToPoint(0, 0, 2000, {.maxSpeed = 50}, false);
+    intake.brake();
+    chassis.moveToPoint(-23.5, -23.5, 1500, {.maxSpeed = 80});
+    intake.move_velocity(400);
+    chassis.turnToPoint(-59, -48, 500, {.maxSpeed = 90}, false);
+    chassis.moveToPoint(-59, -48, 1000, {.minSpeed = 20}, false);
+    chassis.moveToPoint(-44, -64, 1200, {.maxSpeed = 65}, false);
+
+    // place goal in corner 
+    chassis.turnToPoint(-65, 65, 600, {.forwards = false, .maxSpeed = 95}, false);
+    chassis.moveToPoint(-65, 65, 1000, {.forwards = false, .maxSpeed = 70}, false);
+    intake.brake();
+    toggle_clamp();
+
+    move_arm_to(22, 800);
+    intake.move_velocity(350);
+    chassis.moveToPoint(-23.5, -47, 1500, {.maxSpeed = 90});
+
+    // grab goal
+    chassis.turnToPoint(-49, 24, 700, {.forwards = false}, false);
+    chassis.moveToPoint(-49, 24, 1000, {.forwards = false, .maxSpeed = 70, .minSpeed = 5}, false);
+    toggle_clamp();
+
+    chassis.moveToPoint(-0.5, -44, 2000, {.maxSpeed = 80}, false);
+    intake.brake();
+    chassis.turnToPoint(0, 64, 600, {.maxSpeed = 75}, false);
+    chassis.moveToPoint(0, 64, 900, {.maxSpeed = 75}, false);
+    score_high_stake({0, 64, 0});
+    chassis.moveToPoint(0, 48, 600, {.forwards = false, .maxSpeed = 70});
+    move_arm_to(0, 2000);
+
+    intake.move_velocity(350);
+    chassis.moveToPoint(24, -48, 1500, {.maxSpeed = 80}, false);
+    chassis.moveToPoint(48, -48, 1500, {.maxSpeed = 80, .minSpeed = 30});
+    
+
+    pros::lcd::print(5, "left: %u | right: %u", left_motors.get_temperature(), right_motors.get_temperature());
+    pros::lcd::print(5, "arm: %u | intake: %u", arm.get_temperature(), intake.get_temperature());
 }
