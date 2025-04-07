64c34707d057239782dde309ecade886f7f3b8eb
James Fang <james.fang.713@gmail.com>
2025-04-06 22:37:53 -0500

    final 4/6

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,9 @@ void initialize() {
     vertical_encoder.set_data_rate(5);
     horizontal_encoder.set_data_rate(5);
 
-
     chassis.calibrate(); // calibrate sensors
     arm.set_brake_mode_all(pros::E_MOTOR_BRAKE_HOLD);
-    arm_encoder.set_position(0);
-    
-
+    arm_encoder.set_position(0);    
 
     // print position to brain screen
     pros::Task screen_task([&]() {
@@ -65,4 +62,3 @@ void opcontrol() {
 void autonomous() {
     skills();
 }
-
