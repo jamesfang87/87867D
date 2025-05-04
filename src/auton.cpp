4d299ba23a30b18f47e9008ad3244df260d41079
2008rjiang <2008rjiang@gmail.com>
2025-05-03 22:34:49 -0500

    rah redneg

--- src/auton.cpp
+++ src/auton.cpp
@@ -30,44 +30,102 @@ void blue_neg() {
 }
 
 
+ASSET(bluepos1_txt);
+void blue_pos() {
+    //center goal rush
+    chassis.setPose(55, -60, 90);
+    // chassis.follow(bluepos1_txt, 12, 2000, true, true);
+    chassis.moveToPoint(7, -60, 1000, {.forwards = false, .maxSpeed = 80}, false);
+
+    pros::delay(1050);
+
+    sweeper.toggle();
+    // pros::delay(50); 
+
+    chassis.moveToPoint(35, -60, 1000, {.forwards = false, .maxSpeed = 80}, false);
+    sweeper.toggle();
+    pros::delay(30);
+
+    chassis.turnToPoint(70, -70, 800, {.maxSpeed = 90}, true);
+    chassis.moveToPoint(15, -58, 1000, {.forwards = false, .maxSpeed = 60}, false);
+    pros::delay(80);
+    clamp.toggle();
+    pros::delay(40);
+    
+    intake.move_velocity(400);
+    pros::delay(100);
+
+    // //grab other ring 
+    // chassis.turnToPoint(24, -48, 800, {.maxSpeed = 90}, true);
+    // intake.move_velocity(280);
+    // pros::delay(20);
+    // chassis.moveToPoint(25.5, -41.5, 1000, {.forwards = true, .maxSpeed = 50}, false);
+    // intake.brake();
+    // // chassis.moveToPoint(-24, -46, 900, {.forwards = false, .maxSpeed = 70}, false);
+
+    // // drop the goal
+    // chassis.moveToPoint(50, -60, 1500, {.forwards = false, .maxSpeed = 70}, false);
+    // clamp.toggle();
+    
+    // //grab second goal
+    // chassis.turnToPoint(70, -70, 800, {.maxSpeed = 90}, true);
+    // // chassis.follow(bluepos2_txt, 12, 2200, false, false);
+    // pros::delay(20);
+    // clamp.toggle();
+    // pros::delay(20);
+    // intake.move_velocity(400);
+    // pros::delay(700);
+    // intake.brake();
+    // clamp.toggle();
+
+    // //hang
+    // move_arm_to(300, 1500);
+    // arm.brake();
+    // chassis.turnToPoint(0, 0, 800, {.forwards = false, .maxSpeed = 90}, false);
+    // chassis.moveToPoint(7, -7, 1200, {.forwards = false, .maxSpeed = 62}, false);
+    // move_arm_to(0, 1500);
+    // arm.brake(); 
+}
+
+
 ASSET(redneg1_txt);
+ASSET(redneg2_txt);
 void red_neg() {
     //alliance stake
-    chassis.setPose(-60, 10, 215);
+    chassis.setPose(-60, 10, 210);
+    // chassis.setPose(-60, 15, 215);
     move_arm_to(20, 1000);
-    arm.brake();
     pros::delay(150);
     intake.move_velocity(400);
     pros::delay(350);
     intake.brake();
-    move_arm_to(250, 1500);
-    arm.brake();
+    move_arm_to(200, 1500);
     pros::delay(600);
 
     //mogo
-    chassis.follow(redneg1_txt, 12, 2500, false, false);
+    //chassis.moveToPoint(-56.1, 16.7, 1000, {.forwards = false, .minSpeed = 50}, false);
+    //chassis.moveToPose(-23.5, 23.5, 300, 3000, {.forwards = false, .minSpeed = 5}, false);
+    chassis.follow(redneg1_txt, 8, 2500, false, false);
+    pros::delay(35);
     clamp.toggle();
     move_arm_to(0, 1500);
-    pros::delay(30);
 
-    //ring 1
-    chassis.turnToPoint(-22, 45, 800, {.maxSpeed = 90}, false);
-    intake.move_velocity(400);
-    chassis.moveToPoint(-22, 43, 1000, {.forwards = true, .maxSpeed = 70}, false);
-    pros::delay(15);
-    chassis.moveToPoint(-22, 36, 1000, {.forwards = false, .maxSpeed = 50}, false);
+    chassis.turnToPoint(-8, 38, 800, {.earlyExitRange = 1}, false);
+    chassis.moveToPoint(-8, 38, 700, {.maxSpeed = 70, .earlyExitRange = 0.1}, false);
 
-    //ring 2 and 3
-    chassis.turnToPoint(-5, 38, 800, {.maxSpeed = 90}, false);
-    chassis.moveToPoint(-5, 38, 1000, {.forwards = true, .maxSpeed = 50}, false);
-
-    chassis.moveToPoint(-15, 40, 1000, {.forwards = false, .maxSpeed = 60}, false);
-
-    chassis.turnToPoint(-3, 46, 800, {.maxSpeed = 90}, false);
-    intake.move_velocity(400);
-    chassis.moveToPoint(-3, 46, 1000, {.forwards = true, .maxSpeed = 50}, false);
+    intake.move_velocity(450);
+    chassis.turnToPoint(-9, 65, 600, {.maxSpeed = 60}, false);
+    chassis.moveToPoint(-10, 65, 1500, {.maxSpeed = 60, .earlyExitRange = 0.1}, false);
 
     
+    // pros::delay(30);
+
+    // // ring 1 and 2
+    // chassis.turnToPoint(-14, 32, 800, {.minSpeed = 50, .earlyExitRange = 1}, false);
+    // chassis.moveToPoint(-14, 32, 1000, {.minSpeed = 60, .earlyExitRange = 0.1}, false);
+
+    // intake.move_velocity(450);
+    // chassis.follow(redneg2_txt, 6, 1500, false, false);
 }
 
 
@@ -122,28 +180,14 @@ void red_pos() {
     //hang
     move_arm_to(300, 1500);
     arm.brake();
-    chassis.turnToPoint(0, 0, 800, {.forwards = false, .maxSpeed = 90}, false);
-    chassis.moveToPoint(-7, -7, 1200, {.forwards = false, .maxSpeed = 62}, false);
+    chassis.turnToPoint(-9, -3, 800, {.forwards = false, .maxSpeed = 90}, false);
+    chassis.moveToPoint(-9, -3, 1200, {.forwards = false, .maxSpeed = 62}, false);
     move_arm_to(0, 1500);
     arm.brake(); 
 
 }
  
 
-ASSET(bluepos1_txt);
-void blue_pos() {
-    chassis.setPose(63, -36, 90);
-    chassis.follow(bluepos1_txt, 12, 3000, false, false);
-    clamp.toggle();
-
-    intake.move_velocity(400);
-    pros::delay(1000);
-    chassis.moveToPoint(24, -48, 3000, {.maxSpeed = 50}, false);
-    intake.brake();
-
-    
-    chassis.moveToPoint(11, -27, 3000, {.maxSpeed = 60}, false);
-}
 
 
 ASSET(blueelim1_txt);
